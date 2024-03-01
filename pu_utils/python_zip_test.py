9d5a2a9a55af381b0b516c3fa66bb65ab941a20f
Chau Duong <chau.duong@ptnglobalcorp.com>
2024-03-01 17:07:44 +0700

    feat: Add reproducible_zip

--- pu_utils/python_zip_test.py
+++ pu_utils/python_zip_test.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+from shutil import rmtree
 
 from ward import test
 
-from pu_utils.python_zip import pip_install
+from pu_utils.python_zip import pip_install, reproducible_zip
 
 PROJECT_DIR = Path(__file__).parents[1]
 FIXTURES_DIR = PROJECT_DIR.joinpath("fixtures")
@@ -21,3 +22,19 @@ def _() -> None:
     target = TMP_DIR.joinpath("python-zip-uv-pip-install-test")
     pip_install(FIXTURES_DIR.joinpath("python_zip_requirements.txt"), target)
     assert target.joinpath("attrs").exists()
+
+
+@test("reproducible_zip() produces identical zip file when its content are the same")
+def _() -> None:
+    # Reproduce the same directory and zip it
+    zip_path = TMP_DIR.joinpath("python_zip.repro.zip")
+    tmp_dir = TMP_DIR.joinpath("repro")
+    tmp_dir.mkdir(0o755, exist_ok=True)
+    tmp_dir.joinpath("repro1.txt").write_bytes(b"First")
+    tmp_dir.joinpath("repro2.txt").write_bytes(b"Second")
+    # Prepare another directory with 2 files of the same content
+
+    reproducible_zip(zip_path, tmp_dir)
+    rmtree(tmp_dir.resolve())
+    golden_path = FIXTURES_DIR.joinpath("repro.zip")
+    assert Path(golden_path).read_bytes() == Path(zip_path).read_bytes()
